2f9ab4f0404215f71b2b351a56748dd107c698b3
unknown <etikarastogi101@gmail.com>
2020-09-06 13:16:30 +0530

    Add weather content

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -7,8 +7,7 @@ const forecast=(latitude,longitude,callback)=>{
            }else if(body.error){
              callback('Unable to find location. Try another search',undefined)
            }else{
-             callback(undefined,body.current.weather_descriptions[0]+'. It is currently '+body.current.temperature+' temparture outside. It feels like '+body.current.feelslike
-            )
+             callback(undefined,body.current.weather_descriptions[0]+'. It is currently '+body.current.temperature+' temparture outside. It feels like '+body.current.feelslike+'. The humidity and pressure are '+body.current.humidity+' '+body.current.pressure+' respectively')
           }
   
         })
